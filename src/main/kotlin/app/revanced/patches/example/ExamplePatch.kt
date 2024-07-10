565f75ac2c9fa4e16a8a9edb0f19ae5cd8a94d17
ushastoe <40743392+krolchonok@users.noreply.github.com>
2024-07-10 11:45:48 +0300

    Update ExamplePatch.kt

--- src/main/kotlin/app/revanced/patches/example/ExamplePatch.kt
+++ src/main/kotlin/app/revanced/patches/example/ExamplePatch.kt
@@ -9,12 +9,62 @@ import app.revanced.patcher.patch.annotation.Patch
     name = "Example Patch",
     description = "This is an example patch to start with.",
     compatiblePackages = [
-        CompatiblePackage("org.telegram.messenger", ["10.14.4"]),
+        CompatiblePackage("org.telegram.messenger"),
     ],
 )
 @Suppress("unused")
 object ExamplePatch : BytecodePatch(emptySet()) {
+
     override fun execute(context: BytecodeContext) {
-        // TODO("Not yet implemented")
+        // Find all api call to isChatNoForwards in MessagesController.
+        buildMap {
+            context.classes.forEach { classDef ->
+                classDef.methods.let { methods ->
+                    buildMap methodList@{
+                        methods.forEach methods@{ method ->
+                            with(method.implementation?.instructions ?: return@methods) {
+                                ArrayDeque<Int>().also { patchIndices ->
+                                    this.forEachIndexed { index, instruction ->
+                                        if (instruction.opcode != Opcode.INVOKE_VIRTUAL) return@forEachIndexed
+
+                                        val methodRef =
+                                            (instruction as Instruction35c).reference as MethodReference
+                                        if (methodRef.definingClass != "Lorg/telegram/messenger/MessagesController;" || methodRef.name != "isChatNoForwards") return@forEachIndexed
+
+                                        patchIndices.add(index)
+                                    }
+                                }.also { if (it.isEmpty()) return@methods }.let { patches ->
+                                    put(method, patches)
+                                }
+                            }
+                        }
+                    }
+                }.also { if (it.isEmpty()) return@forEach }.let { methodPatches ->
+                    put(classDef, methodPatches)
+                }
+            }
+        }.forEach { (classDef, methods) ->
+            with(context.proxy(classDef).mutableClass) {
+                methods.forEach { (method, patches) ->
+                    with(findMutableMethodOf(method)) {
+                        while (!patches.isEmpty()) {
+                            val index = patches.removeLast()
+                            replaceWithFalse(index)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Replace the method call with returning false
+    private fun MutableMethod.replaceWithFalse(index: Int) {
+        addInstructions(
+            index,
+            """
+                const/4 v0, 0x0
+                move-result v0
+            """,
+        )
     }
 }
